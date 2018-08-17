5ff93632a099660f657c1806e7fa74fc4fabf4bb
Lara <lara.ej@gmail.com>
2018-08-17 23:58:26 +0100

    Handling missing bookshelf

--- src/Book.js
+++ src/Book.js
@@ -16,7 +16,7 @@ class Book extends React.Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>
           <div className="book-shelf-changer">
-            <select defaultValue={ this.props.book.shelf } onChange={ this.move }>
+            <select defaultValue={ this.props.book.shelf ? this.props.book.shelf : "none" } onChange={ this.move }>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
