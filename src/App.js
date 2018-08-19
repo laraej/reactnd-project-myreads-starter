1c6df88b0b342359193e137dcb8a473fc03f67b3
Lara <lara.ej@gmail.com>
2018-08-19 12:41:21 +0100

    Clearing search results between searches

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <a onClick={() => this.setState({ showSearchPage: true, searchResults: [] })}>Add a book</a>
             </div>
           </div>
         )}
