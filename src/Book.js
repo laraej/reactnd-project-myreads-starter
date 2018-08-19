98f3fba908b93309308de1f8d652fc066ddbd8bb
Lara <lara.ej@gmail.com>
2018-08-19 12:35:14 +0100

    Handling missing covers

--- src/Book.js
+++ src/Book.js
@@ -10,10 +10,18 @@ class Book extends React.Component {
     this.props.onMove(this.props.book, event.target.value)
   }
   render() {
+    const coverStyle = {
+      width: 128,
+      height: 192,
+      backgroundColor: '#cccccc'
+    };
+    if (this.props.book.imageLinks)
+      coverStyle.backgroundImage = 'url(' + this.props.book.imageLinks.thumbnail + ')'
+
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>
+          <div className="book-cover" style={ coverStyle }></div>
           <div className="book-shelf-changer">
             <select defaultValue={ this.props.shelf ? this.props.shelf : "none" } onChange={ this.move }>
               <option value="move" disabled>Move to...</option>
